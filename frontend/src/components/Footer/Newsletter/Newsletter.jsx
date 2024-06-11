5e97011ca79784da1cac80698d4724da8fd6cb92
Om Jadav <omjadav04@gmail.com>
2024-06-11 19:00:39 +0530

    Footer,Newsletter added

--- frontend/src/components/Footer/Newsletter/Newsletter.jsx
+++ frontend/src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,41 @@
 import "./Newsletter.scss";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+} from "react-icons/fa";
+
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <div className="newsletter-section">
+      <div className="newsletter-content">
+        <span className="small-text">News Letter</span>
+        <span className="big-text">Sign up for latest udpates and offers</span>
+        <div className="form">
+          <input type="text" placeholder="Enter Your Email Address" />
+          <button>Subscribe</button>
+        </div>
+        <div className="text">
+          Will be used in accordance with our Privacy Policy
+        </div>
+        <div className="social-icons">
+          <div className="icon">
+            <FaFacebookF size={14} />
+          </div>
+          <div className="icon">
+            <FaInstagram size={14} />
+          </div>
+          <div className="icon">
+            <FaLinkedinIn size={14} />
+          </div>
+          <div className="icon">
+            <FaTwitter size={14} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
--- frontend/src/components/Home/Banner/Banner.jsx
+++ frontend/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,26 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>SALES</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod
+            explicabo molestiae unde fugiat impedit quo eveniet mollitia, itaque
+            adipisci praesentium?
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
+        </div>
+        <img src={BannerImg} className="banner-img" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
