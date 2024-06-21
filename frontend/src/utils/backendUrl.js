4b798cfa9c873afd7c3bada7bd39701809661551
Om Jadav <omjadav04@gmail.com>
2024-06-21 13:40:24 +0530

    backendurl changed

--- frontend/src/utils/backendUrl.js
+++ frontend/src/utils/backendUrl.js
@@ -1,4 +1,5 @@
 
-const backendUrl = "http://localhost:5000";
+// const backendUrl = "http://localhost:5000";
+const backendUrl = "https://ecommerce-app-backend-eosin.vercel.app";
 
 module.exports = backendUrl
